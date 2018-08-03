add40a1c33af2eda52da5d8794b295ee0da82b3d
Andrey10 <matweewa.yek@mail.ru>
2018-08-03 13:55:47 +0300

    new rep

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: 'an.kutepoff@mail.ru'
+  layout 'mailer'
+end
---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'users/new'
+
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  get   '/logout', to: 'sessions#destroy'
+  delete '/logout',  to: 'sessions#destroy'
+  delete '/users', to: 'users#destroy'
+  resources :users
+  resources :account_activations, only: [:edit]
+end
