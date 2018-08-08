4c9d20d2c581b911e5e74be3696a7d443b301130
Andrey10 <matweewa.yek@mail.ru>
2018-08-08 21:19:35 +0300

    add all

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,11 @@ Rails.application.routes.draw do
   get   '/logout', to: 'sessions#destroy'
   delete '/logout',  to: 'sessions#destroy'
   delete '/users', to: 'users#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
---
+++ test/models/relationship_test.rb
@@ -0,0 +1,7 @@
+require './test/test_helper'
+
+class RelationshipTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
